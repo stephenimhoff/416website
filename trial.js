5936e3907f7e8635bf83f8988df4ca4d29f9208c
Stephen Imhoff <76451078+stephenimhoff@users.noreply.github.com>
2023-07-27 17:28:47 -0400

    Add files via upload

--- trial.js
+++ trial.js
@@ -175,7 +175,7 @@ d3.csv('https://stephenimhoff.github.io/416website/Networth-Levels-Final.csv').t
 
   function explore(){
 
-    location.href = "www.stephenimhoff.github.io/416website/dashboardv2.html:";
+    location.href = "dashboardv2.html:";
   }
   
   function update() {
